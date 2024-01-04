d27d47548b0cfbf63d608d0feae8d03da77b08c5
ANANDBABU <ANANDBABU@Rev-PF2ZN23S>
2024-01-04 08:17:45 -0800

    try this

---
+++ myproject/src/main/java/com/myproject/Calculator.java
@@ -0,0 +1,15 @@
+package com.myproject;
+
+public class Calculator {
+	public int addition (int i, int j) {
+		return i+j;
+	}
+	public int substraction (int i, int j) {
+		return i-j;
+	}public int multiplication (int i, int j) {
+		return i*j;
+	}public int division (int i, int j) {
+		return i/j;
+	}
+
+}
